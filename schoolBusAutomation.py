f598e0d917dc86db7feceee2b49e5e114a373f32
sakib <sakibbd.cse@gmail.com>
2024-11-23 22:06:03 +0600

    school bus automation

--- schoolBusAutomation.py
+++ schoolBusAutomation.py
@@ -3,9 +3,16 @@ class Vehicle:
         self.max_speed = max_speed
         self.milage = milage
 
-class Bus(Vehicle):
-    pass  
+    def sitting_capacity(self, capacity):
+        return f"Sitting capacity of the vehicle is {capacity} passengers."
 
+class Bus(Vehicle):
+    def sitting_capacity(self, capacity=50):
+        return f"Sitting capacity of the bus is {capacity} passengers."
+
+# Creating a Bus object
 school_bus = Bus(70, 20)
 
+# Displaying the sitting capacity and other attributes
+print(school_bus.sitting_capacity())  # Default capacity is 50
 print(school_bus.max_speed, school_bus.milage)
